b388941e377ace5cc20682a29d13333660e96fbe
vivienlherondelle <113612627+vivienlherondelle@users.noreply.github.com>
2022-12-01 16:51:32 +0100

    Delete compteur.js

--- js/compteur.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#text").addEventListener('keyup', function (){
-    let mot = document.getElementById('text').value
-        document.getElementById('result').innerHTML =not.length;
-    
-});
