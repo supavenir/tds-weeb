7f67b1ee755f4b7fe558ffda2fd40c3a90131651
vivienlherondelle <113612627+vivienlherondelle@users.noreply.github.com>
2022-12-01 16:49:50 +0100

    Add files via upload

---
+++ js/compteur.js
@@ -0,0 +1,5 @@
+document.querySelector("#text").addEventListener('keyup', function (){
+    let mot = document.getElementById('text').value
+        document.getElementById('result').innerHTML =not.length;
+    
+});
